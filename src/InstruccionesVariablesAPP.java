1829b97e6c943944d1894026c1957f54ac89a560
AlbertNoMes <albertnotariomestres@gmail.com>
2022-01-12 23:14:23 +0100

    Instrucciones con 1 variable

---
+++ src/InstruccionesVariablesAPP.java
@@ -0,0 +1,17 @@
+
+public class InstruccionesVariablesAPP {
+
+	public static void main(String[] args) {
+		
+		int varN=10;
+		
+		System.out.println("La variable N es "+varN);
+		varN+=77;
+		System.out.println("N + 77 = "+varN);
+		varN-=3;
+		System.out.println("N - 3 = "+varN);
+		System.out.println("El doble de N es = "+(varN*2));
+
+	}
+
+}
